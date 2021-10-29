dfc0168ee2b53863edf9a91b7a411dd7b5e9d311
Ex <ex@ex-linux>
2021-10-29 17:19:05 -0400

    Add More Warning Constants

--- autoloads/Warnings.gd
+++ autoloads/Warnings.gd
@@ -7,3 +7,8 @@ const EMPTY_PREFIX := "There is no file prefix. Please provide a file prefix"
 const NO_VOICE_SELECTED := "There is no voice selected. Please select a voice."
 const NO_DIRECTORY_SELECTED := "No directory selected. Please select a folder."
 const NO_FILE_SELECTED := "No file name provided. Please provide a file name."
+const INVALID_URL := "Invalid URL please provide valid URL."
+const INVALID_URL_REQUEST := "Site returned Error 404.\nCheck that you are request the correct URL."
+const INVALID_API_KEY := "Site returned Error 401.\nCheck that your API key is correct."
+
+const UNKNOWN_ERROR := "An unknown error occured."
